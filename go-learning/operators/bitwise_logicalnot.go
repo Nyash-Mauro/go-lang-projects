0d132578d8ca98727b98247ce00b817ecaebade5
Nyash-Mauro <mauricee423@gmail.com>
2022-11-02 17:54:56 +0300

    learned multiplication and addition

---
+++ go-learning/operators/bitwise_logicalnot.go
@@ -0,0 +1,13 @@
+// ^ bitwise complement and ! logical NOT: 
+
+package main
+import "fmt"
+
+func main()  {
+// bitwise complement
+	var i int = 2
+	fmt.Println("^i =",^i)
+// logical not operator
+	var j bool = true
+	fmt.Println("!j =",!j)
+}
---
+++ go-learning/operators/multiplication_division.go
@@ -0,0 +1,13 @@
+//// multiplication /division operators *,%,/
+package main 
+import "fmt"
+
+func main()  {
+	var i int = 20
+	var j int = 10
+
+	fmt.Println("i * j =",i * j)
+	fmt.Println("i / j =",i / j)
+	fmt.Println("i % j =", i % j)
+	
+}
