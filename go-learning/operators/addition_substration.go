498e17e747086bc1c41b080d42bc3a74a810eea3
Nyash-Mauro <mauricee423@gmail.com>
2022-11-04 11:03:43 +0300

    created exponent operator

---
+++ go-learning/operators/addition_substration.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main()  {
+	var i int = 20
+	var j int = 10
+
+	fmt.Println("i + j =",i + j)
+	fmt.Println("i - j =",i - j)
+}
---
+++ go-learning/operators/exponent_operator.go
@@ -0,0 +1,10 @@
+// exponent operator ^
+package main
+import "fmt"
+
+func main()  {
+	var i int = 10
+	var j int = 5
+
+	fmt.Println("i ^ j =",i ^ j)
+}
