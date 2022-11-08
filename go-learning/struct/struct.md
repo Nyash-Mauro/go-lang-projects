bff05eccb520e904987456a639efd78135adca01
Nyash-Mauro <mauricee423@gmail.com>
2022-11-08 09:19:45 +0300

    created struct

---
+++ go-learning/struct/struct.md
@@ -0,0 +1,3 @@
+###GO STRUCT###
+Struct in Go is a collection of data. We can use it to define custom data types.
+A struct can contain member fields inside them, and can even contain other struct types.
---
+++ handler.py
@@ -0,0 +1,14 @@
+from asyncio import log
+import json
+
+
+
+def handle(event,handle):
+        s = 'Daniel'
+        n = len(s)
+        for i in range(log,n):
+            if s[i] <= s[5]:
+                return{
+                    
+                }
+            
