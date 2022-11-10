2e64c46e7ce7adb707980b3fe843d29faf556278
Nyash-Mauro <mauricee423@gmail.com>
2022-11-10 15:49:47 +0300

    created fu server and router plus handler func

--- load-balancer/loadbalancer/loadbalancer.go
+++ load-balancer/loadbalancer/loadbalancer.go
@@ -11,15 +11,39 @@ import (
 var (
 	baseURL = "http://localhost:808"
 )
+
 type LoadBalancer struct {
 	RevProxy httputil.ReverseProxy
 }
 
-type EndPoints struct{
+type EndPoints struct {
 	List []*url.URL
 }
- func (e *EndPoints) Shuffle() {
+
+func (e *EndPoints) Shuffle() {
 	temp := e.List[0]
 	e.List = e.List[1:]
 	e.List = append(e.List, temp)
- }
+}
+
+func MakeLoadBalancer(amount int) {
+	// Instantiate Objects
+	var lb LoadBalancer
+	var ep Endpoints
+
+	// server and router
+	router := http.NewServeMux()
+	server := http.Serve{
+		Addr:    ":8090",
+		Handler: router,
+	}
+	// Creating the endpoints
+	for i := 0; i < amount; i++ {
+		ep.List = append(ep.List, createEndpoint(baseURL, i))
+	}
+
+	// Handler Functions
+	router.HandleFunc("/loadbalancer", makeRequest(&lb, &ep))
+
+	// Listen and Server
+}
