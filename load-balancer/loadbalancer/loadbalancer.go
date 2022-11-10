e4fde88d355669dab94c683047e8a8b0af20d435
Nyash-Mauro <mauricee423@gmail.com>
2022-11-10 16:05:29 +0300

    finished up the functionality of the load balancer

--- load-balancer/loadbalancer/loadbalancer.go
+++ load-balancer/loadbalancer/loadbalancer.go
@@ -16,11 +16,11 @@ type LoadBalancer struct {
 	RevProxy httputil.ReverseProxy
 }
 
-type EndPoints struct {
+type Endpoints struct {
 	List []*url.URL
 }
 
-func (e *EndPoints) Shuffle() {
+func (e *Endpoints) Shuffle() {
 	temp := e.List[0]
 	e.List = e.List[1:]
 	e.List = append(e.List, temp)
@@ -31,12 +31,13 @@ func MakeLoadBalancer(amount int) {
 	var lb LoadBalancer
 	var ep Endpoints
 
-	// server and router
+	// Server + Router
 	router := http.NewServeMux()
-	server := http.Serve{
+	server := http.Server{
 		Addr:    ":8090",
 		Handler: router,
 	}
+
 	// Creating the endpoints
 	for i := 0; i < amount; i++ {
 		ep.List = append(ep.List, createEndpoint(baseURL, i))
@@ -49,20 +50,32 @@ func MakeLoadBalancer(amount int) {
 	log.Fatal(server.ListenAndServe())
 }
 
-
-
-func makeRequest(lb *LoadBalancer,ep *EndPoints) func(w http.ResponseWriter,r *http.Request){
-	return func(w http.ResponseWriter, r *http.Request){
-		for !testServer(ep.List[0].String()){
+func makeRequest(lb *LoadBalancer, ep *Endpoints) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		for !testServer(ep.List[0].String()) {
 			ep.Shuffle()
 		}
 		lb.RevProxy = *httputil.NewSingleHostReverseProxy(ep.List[0])
 		ep.Shuffle()
-		lb.RevProxy.ServeHTTP(w,r)
+		lb.RevProxy.ServeHTTP(w, r)
 	}
 }
-func createEndpoint(endpoint string,idx int) *url.URL {
+
+func createEndpoint(endpoint string, idx int) *url.URL {
 	link := endpoint + strconv.Itoa(idx)
-	url,_ :=url.Parse(link)
+	url, _ := url.Parse(link)
 	return url
+}
+
+func testServer(endpoint string) bool {
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		return false
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return false
+	}
+
+	return true
 }
