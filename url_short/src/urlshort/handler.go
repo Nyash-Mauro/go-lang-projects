b64204ceafbb5060a1ca5fa7e04058bce1795ba4
Nyash-Mauro <mauricee423@gmail.com>
2022-11-12 13:27:34 +0300

    function to map yaml url

--- url_short/src/urlshort/handler.go
+++ url_short/src/urlshort/handler.go
@@ -13,3 +13,28 @@ func NewBaseUrlMapper(urls map[string]string) func(string) (string, bool) {
 		return url, ok
 	}
 }
+
+
+func NewYamlUrlMapper(filename string) (func(string) (string, bool), error) {
+	content, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	yml := []map[string]string{}
+	err = yaml.Unmarshal(content, &yml)
+
+	if err != nil {
+		return nil, err
+	}
+
+	mapping := make(map[string]string)
+
+	for _, m := range yml {
+		mapping[m["path"]] = m["url"]
+	}
+
+	return NewBaseUrlMapper(mapping), nil
+}
+
